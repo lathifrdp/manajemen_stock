9d5aed8040dd75138d7b6f2170a7dfbca845b2df
lathifrdp <lathifrdp@gmail.com>
2018-06-04 14:43:52 +0700

    form registrasi

--- ManajemenBarang/Controllers/DashboardController.cs
+++ ManajemenBarang/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ManajemenBarang.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,17 +10,27 @@ namespace ManajemenBarang.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
+        dbStokEntities dbe = new dbStokEntities();
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult toAdmin()
-        {
-            return View();            
-        }
-        public ActionResult toSupplier()
+        public ActionResult Register()
         {
             return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult LoginData()
+        {
+            return View();
+        }
+        public ActionResult RegisterData(GetUsup_Result usup)
+        {
+            dbe.AddUser(usup.id_user, usup.username, usup.password, usup.status_user, usup.nama_supplier, usup.alamat_supplier, usup.telp_supplier);
+            return View("RegisterData");            
         }
     }
 }
---
+++ ManajemenBarang/Models/RegModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManajemenBarang.Models
+{
+    public class RegModel
+    {
+        dbStokEntities dbe = new dbStokEntities();
+        //public List<tbUser> GetUsers
+        //{
+            //return 
+        //}
+    }
+}
