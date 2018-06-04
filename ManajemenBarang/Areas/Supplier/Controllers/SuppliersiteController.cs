98ae2f75143a3b2320e4df5d8785baf3672bdb88
lathifrdp <lathifrdp@gmail.com>
2018-06-04 08:56:10 +0700

    create area

---
+++ ManajemenBarang/Areas/Supplier/Controllers/SuppliersiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManajemenBarang.Areas.Supplier.Controllers
+{
+    public class SuppliersiteController : Controller
+    {
+        // GET: Supplier/Suppliersite
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ManajemenBarang/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManajemenBarang.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult toAdmin()
+        {
+            return View();            
+        }
+        public ActionResult toSupplier()
+        {
+            return View();
+        }
+    }
+}
