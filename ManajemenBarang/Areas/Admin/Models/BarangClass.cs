8b77e009f19fe7e4726163ee704e1f4a72132b23
Riffan Alfarizie <riffanalfa@gmail.com>
2018-06-07 13:04:45 +0700

    Model Barang, List

---
+++ ManajemenBarang/Areas/Admin/Models/BarangClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManajemenBarang.Areas.Admin.Models
+{
+    public class BarangClass
+    {
+        dbStokEntities entities = new dbStokEntities();
+        public List<spGetBarangJoin_Result> getBarang()
+        {
+            return entities.spGetBarangJoin().ToList();
+        }
+        public List<spGetBarangMasukJoin_Result> getBarangMasuk()
+        {
+            return entities.spGetBarangMasukJoin().ToList();
+        }
+        public List<spGetBarangKeluarJoin_Result> getBarangKeluar()
+        {
+            return entities.spGetBarangKeluarJoin().ToList();
+        }
+        public List<spGetBarangReturnJoin_Result> getBarangReturn()
+        {
+            return entities.spGetBarangReturnJoin().ToList();
+        }
+
+        public List<spGetBarang_Result> getBrg()
+        {
+            return entities.spGetBarang().ToList();
+        }
+        public List<spGetBarangList_Result> getBarangList()
+        {
+            return entities.spGetBarangList().ToList();
+        }
+        public List<spGetKategori_Result> getKategori()
+        {
+            return entities.spGetKategori().ToList();
+        }
+        public List<spGetUser_Result> getUser()
+        {
+            return entities.spGetUser().ToList();
+        }
+        public List<spGetSupplier_Result> getSupplier()
+        {
+            return entities.spGetSupplier().ToList();
+        }
+        public List<spGetBarangWhere_Result> getEditResult(int id)
+        {
+            return entities.spGetBarangWhere(id).ToList();
+        }
+        public List<spGetSupplierWhere_Result> getSupplierWhere(int id)
+        {
+            return entities.spGetSupplierWhere(id).ToList();
+        }
+        
+        public List<spGetBarangJoinID_Result> getBarangID()
+        {
+            return entities.spGetBarangJoinID().ToList();
+        }
+    }
+}
---
+++ ManajemenBarang/Areas/Admin/Models/ListOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManajemenBarang.Areas.Admin.Models
+{
+    public class ListOptions
+    {
+        public List<spGetBarangKeluar_Result> getKeluar { get; set; }
+        public List<spGetBarangJoinID_Result> getBarangID { get; set; }
+        public List<spGetUser_Result> getUser { get; set; }
+        public List<spGetBarangList_Result> getBarang { get; set; }
+        public List<spGetKategori_Result> getKategori { get; set; }
+        public List<spGetSupplier_Result> getSupplier { get; set; }
+        public List<spGetBarangWhere_Result> getEdit { get; set; }
+    }
+}
